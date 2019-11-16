e830429ad87bc6c28cafbeffa8eb8e54a9b751d6
Milos Ratkovic <milosr@gmail.com>
2019-11-16 14:21:49 +0100

    su: added libz to LOCAL_SHARED_LIBRARIES

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := su
 LOCAL_MODULE_TAGS := optional
 LOCAL_WHOLE_STATIC_LIBRARIES := libcutils libutils libbinder liblog libselinux libcrypto_static libvndksupport
-LOCAL_SHARED_LIBRARIES := libbacktrace
+LOCAL_SHARED_LIBRARIES := libbacktrace libz
 LOCAL_SRC_FILES := su.c daemon.c utils.c pts.c
 LOCAL_SRC_FILES += binder/appops-wrapper.cpp binder/pm-wrapper.c
 LOCAL_CFLAGS += -Werror -Wall
